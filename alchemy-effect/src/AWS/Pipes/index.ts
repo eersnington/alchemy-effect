38af3e007c4c77cdfddc7875387291e3214b299c
Sam Goodwin <sam@alchemy.run>
2026-03-12 14:28:04 -0700

    feat(aws,pipes): Pipe resources

---
+++ alchemy-effect/src/AWS/Pipes/index.ts
@@ -0,0 +1,7 @@
+export * from "./Pipe.ts";
+export { dynamodb } from "./dynamodb.ts";
+export type { DynamoDBSourceProps } from "./dynamodb.ts";
+export { kinesis } from "./kinesis.ts";
+export type { KinesisSourceProps } from "./kinesis.ts";
+export { sqs } from "./sqs.ts";
+export type { QueueSourceProps } from "./sqs.ts";
---
+++ alchemy-effect/test/AWS/Pipes/Pipe.test.ts
@@ -0,0 +1,6 @@
+import { describe, test } from "vitest";
+
+describe("Pipe", () => {
+  test.skip("creates a source to target pipe", () => {});
+  test.skip("updates pipe routing parameters", () => {});
+});
